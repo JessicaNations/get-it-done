cafda6e4a65a4f2e4ed0e262884d7d9dcb4df236
Jessica Nations <jessica.nations@gmail.com>
2018-04-07 15:22:46 -0500

    why wont it work!

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Task(db.Model):
         self.name = name
         self.completed = False
 
-class User(db.Model)
+class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
